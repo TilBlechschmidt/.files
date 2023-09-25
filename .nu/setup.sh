1a41d156b3947e366a4766bba5f45ba2d40d1497
Til Blechschmidt <til@blechschmidt.de>
2023-09-25 11:14:41 +0200

    Add clone to setup script

--- .nu/setup.sh
+++ .nu/setup.sh
@@ -1,3 +1,22 @@
+git clone --bare git@github.com:TilBlechschmidt/.files.git $HOME/.cfg
+
+function config {
+   /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
+}
+
+mkdir -p .cfg-backup
+
+config checkout
+if [ $? = 0 ]; then
+  echo "Checked out config.";
+  else
+    echo "Backing up pre-existing dot files.";
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .cfg-backup/{}
+fi;
+
+config checkout
+config config status.showUntrackedFiles no
+
 cargo install nu --features dataframe
 cargo install zoxide
 cargo install atuin

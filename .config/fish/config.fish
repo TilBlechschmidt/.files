8e01158efd12ab34e3f214faa54593a0e0a5c996
Til Blechschmidt <me@tibl.dev>
2024-02-10 15:34:01 +0100

    Add Kubernetes abbreviations

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,4 +7,16 @@ if status is-interactive
     alias cd="z"
 
     # abbr -a cd z
+
+    abbr -a k kubectl
+    abbr -a kg kubectl get
+    abbr -a kgp kubectl get pod
+    abbr -a kgpw kubectl get pod -w
+    abbr -a kd kubectl describe
+    abbr -a kdp kubectl describe pod
+    abbr -a kl kubectl logs
+    abbr -a kc kubectl config
+    abbr -a kdel kubectl delete
+    abbr -a kcns kubectl config set-context --current --namespace 
+    abbr -a tf terraform
 end

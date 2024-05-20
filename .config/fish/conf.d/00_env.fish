e3393a7ceb3bb580caeadf9e5cad1f3ae85f00e2
Til Blechschmidt <me@tibl.dev>
2024-05-20 03:57:00 +0200

    Make `helix` the default editor

--- .config/fish/conf.d/00_env.fish
+++ .config/fish/conf.d/00_env.fish
@@ -5,7 +5,7 @@ set PATH $HOME/.cargo/bin $PATH
 fnm env --use-on-cd | source
 
 # Random stuff
-set -gx EDITOR nvim
+set -gx EDITOR hx
 set -gx GPG_TTY tty
 set -gx BAT_THEME Nord
 set -gx LAUNCH_ENDPOINT "http://10.0.0.56:1200"

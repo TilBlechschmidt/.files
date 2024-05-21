ee46c6015e04846ccc303df1f9f4ee197c1b59c3
Til Blechschmidt <til.blechschmidt@ppi-fs.de>
2024-05-21 10:59:55 +0200

    Fix delta pager scrolling

--- .config/fish/conf.d/00_env.fish
+++ .config/fish/conf.d/00_env.fish
@@ -10,3 +10,7 @@ set -gx GPG_TTY tty
 set -gx BAT_THEME Nord
 set -gx LAUNCH_ENDPOINT "http://10.0.0.56:1200"
 set -gx JJ_CONFIG $HOME/.config/jj/config.toml
+
+# Fix for `delta` pager scrolling
+# https://github.com/dandavison/delta/issues/630#issuecomment-2003149860
+set -gx LESS '-R --mouse'

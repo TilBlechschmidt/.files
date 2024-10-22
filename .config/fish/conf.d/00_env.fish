e996ef591133b8f9474ea41699e48d435c53231c
Til Blechschmidt <me@tibl.dev>
2024-10-22 16:08:34 +0200

    Allow device-specific `jj` config extensions

--- .config/fish/conf.d/00_env.fish
+++ .config/fish/conf.d/00_env.fish
@@ -7,12 +7,16 @@ fish_add_path -P $HOME/.cargo/bin
 # fnm (Generates OS specific paths ...)
 fnm env --use-on-cd | source
 
+# jj: Set the config variable to the directory
+# 	  Allows us to place and use multiple files
+#     Used for device-specific configuration (e.g. at work)
+set -gx JJ_CONFIG $HOME/.config/jj
+
 # Random stuff
 set -gx EDITOR hx
 set -gx GPG_TTY tty
 set -gx BAT_THEME Nord
 set -gx LAUNCH_ENDPOINT "http://10.0.0.56:1200"
-set -gx JJ_CONFIG $HOME/.config/jj/config.toml
 set -gx GRAB_HOME $HOME/Developer
 
 # Fix for `delta` pager scrolling
